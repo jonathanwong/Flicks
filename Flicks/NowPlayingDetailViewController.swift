05046b68f18191e7cf715cdbd7e62b31d17e0b6c
Jonathan Wong <jonwong@outlook.com>
2017-03-31 15:37:44 -0700

    Scrollview in detail view.

--- Flicks/NowPlayingDetailViewController.swift
+++ Flicks/NowPlayingDetailViewController.swift
@@ -14,6 +14,7 @@ class NowPlayingDetailViewController: UIViewController {
     @IBOutlet weak var movieImageView: UIImageView!
     
     var currentMovie: NSDictionary!
+    var movie: Movie?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +23,7 @@ class NowPlayingDetailViewController: UIViewController {
         let overview = currentMovie.value(forKey: "overview") as! String
         let posterPath = currentMovie.value(forKey: "poster_path") as! String
         let url = URL(string: NowPlayingViewController.baseUrl + posterPath)
-        let movie = Movie(title: title, description: overview, imageUrl: url)
+        self.movie = Movie(title: title, description: overview, imageUrl: url)
         
         if url != nil {
             Alamofire.request(url!).responseData(completionHandler: {
@@ -30,6 +31,8 @@ class NowPlayingDetailViewController: UIViewController {
                 self.movieImageView.image = UIImage(data: response.data!)
             })
         }
+        
+        setupDescriptionView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,7 +40,73 @@ class NowPlayingDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setupDescriptionView() {
+        let width = view.frame.width - (view.frame.width * 0.2)
+        let descriptionView = UIView(frame: .zero)
+        descriptionView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionView.backgroundColor = UIColor.darkGray
+        descriptionView.alpha = 0.8
+        descriptionView.layer.masksToBounds = true
+        view.addSubview(descriptionView)
+        
+        let overviewLabel = UILabel(frame: .zero)
+        let font = UIFont.systemFont(ofSize: 17.0, weight: UIFontWeightLight)
+        overviewLabel.translatesAutoresizingMaskIntoConstraints = false
+        overviewLabel.numberOfLines = 0
+        overviewLabel.preferredMaxLayoutWidth = width
+        overviewLabel.font = font
+        overviewLabel.textColor = UIColor.white
+        descriptionView.addSubview(overviewLabel)
+        overviewLabel.text = movie?.description
+        let rect = rectForText(text: (movie?.description)!, font: font, maxSize: CGSize(width: width, height: 1000))
+        
+        let titleLabel = UILabel(frame: .zero)
+        let titleFont = UIFont.systemFont(ofSize: 22.0, weight: UIFontWeightBold)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = titleFont
+        titleLabel.textColor = UIColor.white
+        descriptionView.addSubview(titleLabel)
+        titleLabel.text = movie?.title
+        let titleRect = rectForText(text: (movie?.title)!, font: titleFont, maxSize: CGSize(width: width, height: 1000))
+        
+        let totalRect = CGSize(width: rect.width + titleRect.width + 20, height: rect .height + titleRect.height + 20)
 
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        let viewsDictionary: [String: UIView] = ["descriptionView": descriptionView,
+                               "overviewLabel": overviewLabel,
+                               "titleLabel": titleLabel,
+                               "view": view,
+                               "scrollView": scrollView,
+                               "uitabbar": (self.tabBarController?.tabBar)!]
+        var allConstraints = [NSLayoutConstraint]()
+        allConstraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[titleLabel]-[overviewLabel]-8-|", options: [], metrics: nil, views: viewsDictionary)
+        allConstraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[titleLabel]-8-|", options: [], metrics: nil, views: viewsDictionary)
+        allConstraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[overviewLabel]-8-|", options: [], metrics: nil, views: viewsDictionary)
+        
+        NSLayoutConstraint(item: scrollView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: width).isActive = true
+        NSLayoutConstraint(item: scrollView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: totalRect.height).isActive = true
+        NSLayoutConstraint(item: scrollView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: scrollView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: -44).isActive = true
+        
+        scrollView.addSubview(descriptionView)
+        allConstraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|[descriptionView]|", options: [], metrics: nil, views: viewsDictionary)
+        allConstraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|[descriptionView]|", options: [], metrics: nil, views: viewsDictionary)
+
+        scrollView.contentSize = CGSize(width: totalRect.width, height: totalRect.height - 50.0)
+        
+        NSLayoutConstraint.activate(allConstraints)
+    }
+    
+    func rectForText(text: String, font: UIFont, maxSize: CGSize) -> CGSize {
+        let attrString = NSAttributedString(string: text, attributes: [NSFontAttributeName: font])
+        let rect = attrString.boundingRect(with: maxSize, options: NSStringDrawingOptions.usesLineFragmentOrigin, context: nil)
+        let size = CGSize(width: rect.size.width, height: rect.size.height)
+        
+        return size
+    }
+    
     /*
     // MARK: - Navigation
 
